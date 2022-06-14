b0bceb22348e7a54fbfc8b1842a9f7a122e0bc41
Anselmus44 <ansreyhan@gmail.com>
2022-06-15 01:04:24 +0800

    Renaming songs database into bgldatabase. Update some php files.

--- src/guitar/songchord.php
+++ src/guitar/songchord.php
@@ -19,12 +19,13 @@
 <div class="container" style="position: relative; top:-900px; right: 300px; margin-top:30px;">
   <div class="row">
     <h2 style="color:white;"> <?php echo $results['Title'];?></h2>
-     <div class="col-md-5" style="top:40px; ">
+     <div class="col-md-5" style="top:50px; right:140px">
         <div min-width: 936px;  min-height: 475px;>
         <p style="color:white"> <b>Singer:</b> <?php echo $results['SingerName'];?></p>
         <p style="color:white"> <b>Tuning:</b> <?php echo $results['Tuning'];?></p>
         <p style="color:white"> <b>Capo:</b> <?php echo $results['Capo'];?></p>
         <p style="color:white"> <b>Key:</b> <?php echo $results['GuitarKey'];?></p>
+        
         </div>  
       </div>
     </div>
